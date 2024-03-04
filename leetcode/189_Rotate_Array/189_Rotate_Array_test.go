f6f1f55f42c0814a53ac05590dcb72614a5adebf
Joe5451 <skybluejoe123@gmail.com>
2024-03-04 20:10:31 +0800

    refactor test of 189. Rotate Array

--- leetcode/189_Rotate_Array/189_Rotate_Array_test.go
+++ leetcode/189_Rotate_Array/189_Rotate_Array_test.go
@@ -5,38 +5,38 @@ import (
 	"fmt"
 )
 
-type question189 struct {
-	input189
-	output189
+type question struct {
+	input
+	output
 }
 
-type input189 struct {
+type input struct {
 	nums []int
 	k int
 }
 
-type output189 struct {
+type output struct {
 	nums []int
 }
 
 func Test_Problem189(t *testing.T) {
-	questions := []question189 {
+	questions := []question {
 		{
-			input189{[]int{1, 2, 3, 4, 5, 6, 7}, 3},
-			output189{[]int{5, 6, 7, 1, 2, 3, 4}},
+			input{[]int{1, 2, 3, 4, 5, 6, 7}, 3},
+			output{[]int{5, 6, 7, 1, 2, 3, 4}},
 		},
 		{
-			input189{[]int{-1, -100, 3, 99}, 2},
-			output189{[]int{3, 99, -1, -100}},
+			input{[]int{-1, -100, 3, 99}, 2},
+			output{[]int{3, 99, -1, -100}},
 		},
 	}
 
 	fmt.Println("---------- LeetCode Problem 189 ----------")
 
 	for _, question := range questions {
-		nums := question.input189.nums
-		k := question.input189.k
-		fmt.Printf("input: %v", question.input189)
+		nums := question.input.nums
+		k := question.input.k
+		fmt.Printf("input: %v", question.input)
 		rotate(nums, k)
 		fmt.Printf(", output: %v\n", nums)
 	}
