d5c9233e684ccfbf0c647961e6e2e760d390f925
Joe5451 <skybluejoe123@gmail.com>
2024-03-05 23:34:51 +0800

    refactor to replace white space with tab

--- leetcode/941_Valid_Mountain_Array/941_Valid_Mountain_Array.go
+++ leetcode/941_Valid_Mountain_Array/941_Valid_Mountain_Array.go
@@ -2,43 +2,43 @@ package leetcode
 
 // time complexity is O(n), space complexity is O(1)
 func validMountainArray(arr []int) bool {
-    i := 1
-    for i < len(arr) && arr[i] > arr[i - 1] {
-        i++
-    }
+	i := 1
+	for i < len(arr) && arr[i] > arr[i - 1] {
+		i++
+	}
 
-    if i == 1 || i >= len(arr) {
-        return false
-    }
+	if i == 1 || i >= len(arr) {
+		return false
+	}
 
-    for i < len(arr) && arr[i] < arr[i - 1] {
-        i++
-    }
+	for i < len(arr) && arr[i] < arr[i - 1] {
+		i++
+	}
 
-    if i != len(arr) {
-        return false
-    }
+	if i != len(arr) {
+		return false
+	}
 
-    return true
+	return true
 }
 
 // time complexity is O(n), space complexity is O(1)
 func validMountainArray2(arr []int) bool {
-    isUp, isDown := false, false
+	isUp, isDown := false, false
 
-    for i := 1; i < len(arr); i++ {
-        if !isDown && arr[i] > arr[i - 1] {
-            isUp = true
-            continue
-        }
+	for i := 1; i < len(arr); i++ {
+		if !isDown && arr[i] > arr[i - 1] {
+			isUp = true
+			continue
+		}
 
-        if isUp && arr[i] < arr[i - 1] {
-            isDown = true
-            continue
-        }
+		if isUp && arr[i] < arr[i - 1] {
+			isDown = true
+			continue
+		}
 
-        return false
-    }
+		return false
+	}
 
-    return isUp && isDown
+	return isUp && isDown
 }
